c95f5a8f075fa66b91a34a8b8d6a3ccb00a68d8a
arodriguez <arodriguez@avantic.net>
2023-05-13 11:13:24 +0100

    boiler plate

--- app.js
+++ app.js
@@ -4,20 +4,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const https = require('https');
-const fs = require('fs');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,10 +37,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 
 module.exports = app;
